bac5eb206a03b277b6f74192faca938a0b7180bd
Brent Foster <bcfoster@gmail.com>
2025-02-24 14:39:49 -0800

    remove unused selectors

--- src/app/draft/draft.selectors.ts
+++
@@ -1,6 +0,0 @@
-import { createFeatureSelector, createSelector } from '@ngrx/store';
-import * as fromDraft from '../reducers/draft.reducer';
-
-const selectFeature = createFeatureSelector<fromDraft.State>(fromDraft.featureKey);
-
-export const selectVersion = createSelector(selectFeature, (state) => state.version);
